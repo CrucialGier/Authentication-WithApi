0c04a01b392ebd2f4f94b1a41c0e33be74ee0044
Stewart Cole <stewart@>
2016-09-27 16:31:57 -0700

    Authentication and apis

---
+++ src/Authentication-ExtraExploration/Models/UserManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication_ExtraExploration.Models
+{
+    [Table("userManagers")]
+    public class UserManager
+    {
+        [Key]
+        public int Id;
+        public string AccountSid;
+        public string AuthToken;
+        public virtual Message Messages;
+    }
+}
---
+++ src/Authentication-ExtraExploration/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication_ExtraExploration.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
